d7468b94ca26c2548c9f92db4b3f9a7e40dfbc8a
onurtaskinn <onurtaskin180@gmail.com>
2023-03-14 17:08:06 +0100

    first commit

---
+++ src/components/board.vue
@@ -0,0 +1,8 @@
+<td v-if="editando === persona.id">
+    <button class="btn btn-success" @click="guardarPersona(persona)">&#x1F5AB; Guardar</button>
+    <button class="btn btn-secondary ml-2" @click="cancelarEdicion(persona)">&#x1F5D9; Cancelar</button>
+ </td>
+ <td v-else>
+    <button class="btn btn-info" @click="editarPersona(persona)">&#x1F58A; Editar</button>
+    <button class="btn btn-danger ml-2" @click="$emit('delete-persona', persona.id)">&#x1F5D1; Eliminar</button>
+ </td>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
+
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
